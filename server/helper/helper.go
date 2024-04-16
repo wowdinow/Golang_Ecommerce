73f3a4f735cfb879f131dffee2c6bd996eb4d78b
wowdinow <f.wadino@gmail.com>
2024-04-16 20:57:37 +0700

    First Try

--- server/helper/helper.go
+++ server/helper/helper.go
@@ -1,7 +1,39 @@
 package helper
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
 
-func Help (){
-	fmt.Println("Hello World!")
+	"github.com/gin-gonic/gin"
+)
+
+type content struct {
+	email    string
+	password string
+	username string
+}
+
+func Register(c *gin.Context) {
+	fmt.Println(c.Request.Body)
+	var test1 content
+	decoder := json.NewDecoder(c.Request.Body)
+	err := decoder.Decode(&test1)
+	if err != nil {
+		fmt.Printf("error %s", err)
+		c.JSON(501, gin.H{"error": err})
+	}
+
+	var test2 content
+	err = c.BindJSON(&test2)
+
+	fmt.Printf("Bind Email - Why not Set? %v\n", test2.email)
+	fmt.Printf("Bind Password  - Why not Set? %v\n", test2.password)
+	fmt.Printf("Bind Username  - Why not Set? %v\n", test2.username)
+
+	c.JSON(http.StatusCreated, gin.H{"success": "success"})
+}
+
+func Login(c *gin.Context) {
+	c.JSON(http.StatusOK, "This is Login page")
 }
--- server/main.go
+++ server/main.go
@@ -1,7 +1,16 @@
 package main
 
-import "server/helper"
+import (
+	"server/helper"
 
-func main (){
-	helper.Help()
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+	r.POST("/register", helper.Register)
+
+	r.POST("/login", helper.Login)
+
+	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
